241dd7fbd4795b48f6b7606706aa7fe01b04ddef
Muhammad Selim <muhamadselim70@gmail.com>
2024-06-28 19:50:08 +0300

    Add react-query for data fetching and caching
    This commit adds the react-query library to the project, allowing for efficient data fetching and caching. It also includes the necessary setup code to initialize a QueryClient and wrap the application with a QueryClientProvider. The use of react-query will greatly improve the performance and user experience of the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {} from "@tanstack/react-query-devtools";
 import { merge } from "@/lib/utils";
 
 export default function App() {
   const { i18n } = useTranslation("global");
+  const lang = i18n.language.split("-")[0];
+  const [queryClient] = useState(() => new QueryClient());
 
   useEffect(() => {
     // Set the document language and direction to the user's preferred language
-    document.documentElement.lang = i18n.language;
+    document.documentElement.lang = lang;
     document.documentElement.dir = i18n.dir(i18n.language);
-  }, [i18n]);
+  }, [i18n, lang]);
 
   return (
-    <div className={merge(i18n.language.toLocaleLowerCase() === "ar" ? "font-noto" : "font-macan")}>
-      <h1 className="text-3xl font-bold hover:underline">Hello world!</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={merge(lang.toLocaleLowerCase() === "ar" ? "font-noto" : "font-macan")}>
+        <h1 className="text-3xl font-bold hover:underline">Hello world!</h1>
+      </div>
+    </QueryClientProvider>
   );
 }
