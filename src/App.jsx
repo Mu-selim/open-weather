bc2433d2182134ee00c30a76ff14e312034d7088
Muhammad Selim <muhamadselim70@gmail.com>
2024-06-29 01:08:34 +0300

    Refactor App component and add react-query
    The App component has been refactored to remove unused imports and improve code readability. Additionally, the react-query library has been added to enable efficient data fetching and caching. This change will enhance the performance and user experience of the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { useTheme } from "@/hooks/useTheme";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { merge } from "@/lib/utils";
+import { AppContainer } from "@/components";
 
 export default function App() {
-  const { i18n } = useTranslation("global");
-  const lang = i18n.language.split("-")[0];
+  useTheme();
   const [queryClient] = useState(() => new QueryClient());
-
-  useEffect(() => {
-    // Set the document language and direction to the user's preferred language
-    document.documentElement.lang = lang;
-    document.documentElement.dir = i18n.dir(i18n.language);
-  }, [i18n, lang]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={merge(lang.toLocaleLowerCase() === "ar" ? "font-noto" : "font-macan")}>
-        <h1 className="text-3xl font-bold hover:underline">Hello world!</h1>
-      </div>
+      <AppContainer />
       <ReactQueryDevtools initialIsOpen={false} position="left" buttonPosition="bottom-left" />
     </QueryClientProvider>
   );
