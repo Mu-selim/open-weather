c4525729ffb727e575f23a746e68927495b3d795
Muhammad Selim <muhamadselim70@gmail.com>
2024-06-29 21:14:57 +0300

    refactor: Update i18n.js for better debug logging
    The code changes in this commit update the `i18n.js` file to improve debug logging. The `debug` option is now set to `import.meta.env.DEV` instead of `!isProduction` to align with the development environment. This change enhances the logging functionality and ensures accurate debug information during development.

--- src/i18n.js
+++ src/i18n.js
@@ -6,16 +6,13 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import EN from "@/locales/en.json";
 import AR from "@/locales/ar.json";
 
-// check for production
-const isProduction = import.meta.env.VITE_ENV === "production";
-
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     interpolation: { escapeValue: false },
     fallbackLng: "en",
-    debug: !isProduction,
+    debug: import.meta.env.DEV,
     resources: {
       en: { global: EN },
       ar: { global: AR },
